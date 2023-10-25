4de0a05f49df88f41ae24431127145bc83e9f10c
Bence Balogh <fhs48055@fh-salzburg.ac.at>
2023-10-25 20:43:34 +0200

    first

--- ProjectDjangoticket/DjangoTicket/Customers/core_models.py
+++ ProjectDjangoticket/DjangoTicket/Customers/core_models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
-    objects = CoreProductManager()
+    objects = Core()
 
     class Meta:
         abstract: True
